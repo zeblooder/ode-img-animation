3a8062db0abe526a17b9c594d2030b2ec3d736b0
Zhenggehan <31180310+zeblooder@users.noreply.github.com>
2022-01-10 18:31:55 +0800

    finished metrics

--- metrics.py
+++ metrics.py
@@ -1,82 +1,116 @@
 import numpy as np
-from pytorch_msssim import MS_SSIM #https://github.com/VainF/pytorch-msssim
+from pytorch_msssim import MS_SSIM  # https://github.com/VainF/pytorch-msssim
 from ignite.metrics.gan import FID
 import lpips, torch
 import pandas as pd
+import threading
+from tqdm import tqdm
+
+
 class evaluator:
-    def __init__(self,kp_detector,metrics_lst,size):
-        self.kp_detector=kp_detector
+    def __init__(self, kp_detector, generator, metrics_lst, size):
+        self.kp_detector = kp_detector
+        self.generator = generator
         self.loss_fn = lpips.LPIPS(net='vgg', verbose=False)
         self.ms_ssim_module = MS_SSIM(data_range=255, size_average=True, channel=3)
-        self.m = FID()
-        self.metrics_lst=metrics_lst
-        self.metric_func={
-        'l1':self.L1_norm,
-        'LPIPS':self.LPIPS,
-        'PSNR' :self.MSE,
-        'MS-/SSIM':self.MS_SSIM,
-        'FID':self._FID,
-        'AKD':self.AKD
-    }
-        self.video_cnt=0
-        self.result=np.zeros([size,len(metrics_lst)])
-        self.metric_index={v:k for k,v in dict(enumerate(metrics_lst)).items()}
-        self.FIDpos=self.metrics_lst.index("FID")if "FID" in self.metrics_lst else None
-    @staticmethod
-    def L1_norm(src,pred):
-        return np.sum(np.abs(src-pred))/np.size(src)
+        self.m = FID(device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
+        self.metrics_lst = metrics_lst
+        self.metric_func = {
+            'l1': self.L1_norm,
+            'LPIPS': self.LPIPS,
+            'PSNR': self.MSE,
+            'MS-/SSIM': self.MS_SSIM,
+            'FID': self._FID,
+            'AKD': self._AKD
+        }
+        self.video_cnt = 0
+        self.result = np.zeros([size, len(metrics_lst)])
+        self.metric_index = {v: k for k, v in dict(enumerate(metrics_lst)).items()}
+        self.FIDpos = self.metrics_lst.index("FID") if "FID" in self.metrics_lst else None
+        self.AKDpos = self.metrics_lst.index("AKD") if "AKD" in self.metrics_lst else None
 
-    def LPIPS(self,src,pred):
+    def generate(self, source, driving_frame):
+        source_tensor = torch.tensor(source[np.newaxis].astype(np.float32)).cuda()
         with torch.no_grad():
-            return float(self.loss_fn.forward(torch.tensor(src,dtype=torch.float32), torch.tensor(pred,dtype=torch.float32)))
+            kp_source = self.kp_detector(source_tensor)
+            kp_driving = self.kp_detector(torch.tensor(driving_frame[np.newaxis].astype(np.float32)).cuda())
+            out = self.generator(source_tensor, kp_source=kp_source, kp_driving=kp_driving)
+            kp_prediction = self.kp_detector(out['prediction'])
+        return out['prediction'].data.cpu().numpy()[0], kp_driving['value'].squeeze_().cpu().numpy(), kp_prediction[
+            'value'].squeeze_().cpu().numpy()
+
+    def L1_norm(self, src, pred):
+        self.result[self.video_cnt, self.metric_index['l1']] += np.sum(np.abs(src - pred)) / np.size(src)
+
+    def LPIPS(self, src, pred):
+        with torch.no_grad():
+            self.result[self.video_cnt, self.metric_index['LPIPS']] += float(
+                self.loss_fn.forward(torch.tensor(src, dtype=torch.float32),
+                                     torch.tensor(pred, dtype=torch.float32)))
+
+    def MSE(self, src, pred):
+        self.result[self.video_cnt, self.metric_index['PSNR']] = np.sum((src - pred) ** 2) / np.size(src)
+
+    def MS_SSIM(self, src, pred):
+        self.result[self.video_cnt, self.metric_index['MS-/SSIM']] = float(
+            1 - self.ms_ssim_module(255.0 * torch.tensor(src, dtype=torch.float32).unsqueeze_(0),
+                                    255.0 * torch.tensor(pred, dtype=torch.float32).unsqueeze_(0)))
+
     @staticmethod
-    def MSE(src,pred):
-        return np.sum((src-pred)**2)/np.size(src)
-
-
-    def MS_SSIM(self,src,pred):
-        return float(1 - self.ms_ssim_module(torch.tensor(255*src,dtype=torch.float32).unsqueeze_(0), torch.tensor(255*pred,dtype=torch.float32).unsqueeze_(0)))
-
-    @staticmethod
-    def _FID(src,pred):
+    def _FID(src, pred):
         """
         Placeholder function
         """
         return 0
 
-    def FID(self,src,pred):
+    @staticmethod
+    def _AKD(src, pred):
+        """
+        Placeholder function
+        """
+        return 0
+
+    def FID(self, src, pred):
         """
         Video quality. Input must be video
         """
         self.m.reset()
-        self.m.update((torch.tensor(255*pred,dtype=torch.float32), torch.tensor(255*src,dtype=torch.float32)))
-        return self.m.compute()
+        self.m.update((torch.tensor(pred, dtype=torch.float32) * 255.0, torch.tensor(src, dtype=torch.float32) * 255.0))
+        self.result[self.video_cnt, self.metric_index['FID']] = float(self.m.compute())
 
-    def AKD(self, src, pred):
+    def AKD(self, kp_driving, kp_prediction):
         with torch.no_grad():
-            src = torch.tensor(src[np.newaxis].astype(np.float32))
-            kp_source = self.kp_detector(src)
-            pred = torch.tensor(np.array(pred)[np.newaxis].astype(np.float32))
-            kp_prediction = self.kp_detector(pred)
-            return np.average(np.sqrt(np.sum((kp_source['value'].squeeze_().cpu().numpy()-kp_prediction['value'].squeeze_().cpu().numpy())**2,axis=1)))
+            self.result[self.video_cnt, self.AKDpos] = np.average(
+                np.sqrt(np.sum((kp_driving - kp_prediction) ** 2, axis=1)))
 
-    def evaluate(self,src_video,pred_video):
-        assert len(src_video)==len(pred_video)
-        length=len(src_video)
-        metrics=[0]*len(self.metrics_lst)
-        for src_frame, pred_frame in zip(src_video,pred_video):
-            metrics=[self.metric_func[metric](src_frame,pred_frame) for metric in self.metrics_lst]
-        metrics=[data/length for data in metrics]
+    def evaluate(self, source_img, driving_video):
+        length = len(driving_video)
+        pred_video = []
+        for driving_frame in tqdm(driving_video):
+            pred_frame, kp_driving, kp_prediction = self.generate(source_img, driving_frame)
+            pred_video.append(pred_frame)
+            for metric in self.metrics_lst:
+                t = threading.Thread(target=self.metric_func[metric], args=(driving_frame, pred_frame,))
+                t.start()
+                t.join()
+            if not self.FIDpos is None:
+                t = threading.Thread(target=self.AKD, args=(kp_driving, kp_prediction,))
+                t.start()
+                t.join()
+        self.result[self.video_cnt] /= length
         if not self.FIDpos is None:
-            metrics[self.FIDpos]=float(self.FID(src_video,pred_video))
-
-        self.result[self.video_cnt,[self.metric_index[m] for m in self.metrics_lst]]=metrics
-        self.video_cnt+=1
+            t = threading.Thread(target=self.FID, args=(driving_video, pred_video,))
+            t.start()
+            t.join()
+        self.video_cnt += 1
 
     def get_res_list(self):
-        return [res/self.video_cnt for res in self.result]
+        return [res / self.video_cnt for res in self.result]
 
     def get_res_pd(self):
-        df=pd.DataFrame(self.result, columns=self.metrics_lst)
-        df=df.append(df.mean(axis=0),ignore_index=True)
+        df = pd.DataFrame(self.result, columns=self.metrics_lst)
+        df = df.append(df.mean(axis=0), ignore_index=True)
         return df
+
+    def save_res(self, filename='1.csv'):
+        self.get_res_pd().to_csv(filename)
--- performance.py
+++ performance.py
@@ -1,46 +1,13 @@
-import matplotlib
-
-matplotlib.use('Agg')
-import os, sys
+import os
 from tqdm import tqdm
-
 import imageio
 import numpy as np
 from skimage.transform import resize
 import torch
-from sync_batchnorm import DataParallelWithCallback
-from logger import Logger, Visualizer
-from torch.utils.data import DataLoader
-
-from animate import normalize_kp
 from metrics import evaluator
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,5,7'
 
-def make_animation(source_image, driving_video, generator, kp_detector, relative=True, adapt_movement_scale=True,
-                   cpu=False):
-    with torch.no_grad():
-        predictions = []
-        source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
-        if not cpu:
-            source = source.cuda()
-        driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 2, 1, 3, 4)
-        kp_source = kp_detector(source)
-        kp_driving_initial = kp_detector(driving[:, :, 0])
 
-        for frame_idx in tqdm(range(driving.shape[2])):
-            driving_frame = driving[:, :, frame_idx]
-            if not cpu:
-                driving_frame = driving_frame.cuda()
-            kp_driving = kp_detector(driving_frame)
-            # kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
-            #                        kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
-            #                        use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
-            out = generator(source, kp_source=kp_source, kp_driving=kp_driving)
-
-            predictions.append(out['prediction'].data.cpu().numpy()[0])
-    return predictions
-
-def video2imgLst(video,transpose=True):
+def video2imgLst(video):
     reader = imageio.get_reader(video)
     fps = reader.get_meta_data()['fps']
     imgLst = []
@@ -50,35 +17,27 @@ def video2imgLst(video,transpose=True):
     except RuntimeError:
         pass
     reader.close()
-    if transpose:
-        imgLst = [resize(frame, (256, 256))[..., :3].transpose(2,0,1) for frame in imgLst]
-    else:
-        imgLst = [resize(frame, (256, 256))[..., :3] for frame in imgLst]
-    return imgLst[:2]
+    imgLst = [resize(frame, (256, 256))[..., :3].transpose(2, 0, 1) for frame in imgLst]
+    return imgLst
 
-def gen_video(source_image,driving_video,generator,kp_detector,relative=True, adapt_scale=True, cpu=False):
-    driving_video=video2imgLst(driving_video)
-    source_image = resize(source_image, (256, 256))[..., :3]
-    predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=relative,
-                                     adapt_movement_scale=adapt_scale, cpu=cpu)
-    return driving_video, predictions
 
-def performance(generator, kp_detector, dataset, metrics,source_image=None):
-
+def performance(generator, kp_detector, dataset, metrics, source_image=None):
     if source_image is None:
         rand_video = dataset.__getitem__(np.random.randint(len(dataset)))['video']
-        source_image = rand_video[:,np.random.randint(rand_video.shape[1]),:,:]
+        source_image = rand_video[:, np.random.randint(rand_video.shape[1]), :, :]
     else:
         source_image = imageio.imread(source_image)
     if torch.cuda.is_available():
-        generator = DataParallelWithCallback(generator)
-        kp_detector = DataParallelWithCallback(kp_detector)
+        # generator = DataParallelWithCallback(generator)
+        generator = generator.cuda()
+        # kp_detector = DatarallelWithCallback(generator)
+        kp_detector = kp_detector.cuda()
 
     generator.eval()
     kp_detector.eval()
-    e=evaluator(kp_detector,metrics,len(dataset))
+    e = evaluator(kp_detector, generator, metrics, len(dataset))
 
     for it, x in tqdm(enumerate(dataset.videos)):
-        src, pred=gen_video(source_image,os.path.join(dataset.root_dir,x),generator,kp_detector)
-        e.evaluate(src,pred)
-    e.get_res_pd().to_csv('1.csv')
+        driving_img_lst = video2imgLst(os.path.join(dataset.root_dir, x))
+        e.evaluate(source_image, driving_img_lst)
+    e.save_res()
